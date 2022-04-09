b1df0def56ebc85c9077961b8d5b2091b5effbd0
Dmitry <krdvmail@mail.ru>
2022-04-09 12:49:58 +0300

    upgrade project

--- js/addition.js
+++ js/addition.js
@@ -37,102 +37,34 @@ document.onscroll = (e) => {
 /*************Конец блока*****************/
 
 
-/* Работа с кнопкой Очистить */
-/* Показать кнопку Очистить только если что-то выбрано */
-const inputsBtnBlues = document.querySelectorAll('.inputs__btn--blue');
-let flag = 'any';
-const tabs1 = document.querySelector('.tabs__1'); //типы поиска
-const tabs1Btns = tabs1.querySelectorAll('button'); //кнопки типов поиска
-for (let btn of tabs1Btns) {
-  btn.addEventListener('click', handlerTabs1); //обработка нажатия 
+/**********Управление кнопками выбора типа поиска**********/
+const chooseWork = document.querySelector('.choose__work');
+const chooseStaff = document.querySelector('.choose__staff');
+const chooseAny = document.querySelector('.choose__any');
+
+const tabsTitles = document.querySelectorAll('.tabs__title');
+for (let btn of tabsTitles) {
+  btn.addEventListener('click', changeSearchType);
 }
 
-const inputsLabelSel = document.querySelectorAll('.inputs__label--sel');
-
-for (let inp of inputsLabelSel) {
-  inp.onclick = whereClick;
-}
-
-function handlerTabs1(e) {
-  for (let inp of inputsLabelSel) {
-    inp.onclick = whereClick;
-  } //Показать кнопку Очистить при нажатьии на любой select
-
-  if (e.target.classList.contains('staff')) { //Поиск сотрудников
-    flag = 'staff';
-    const inputsRow = document.querySelector('.inputs__row--1');
-    const rowInputs = inputsRow.querySelectorAll('input');
-    const selectRegionSubmit = document.getElementById('select__region-submit'); //кнопка "Выбрать" регион в попапе
-    const regions = document.querySelectorAll('input[name="select__region"]');
-
-    selectRegionSubmit.onclick = () => {
-      for (let item of regions) {
-        if (item.checked) {
-          showClean();
-        }
-      }
-    };
-    for (let inp of rowInputs) {
-      inp.addEventListener('input', showClean);
-    }
+function changeSearchType(e) {
+  for (let btn of tabsTitles) {
+    btn.classList.remove('_tab-active');
+    e.target.classList.add('_tab-active');
   }
 
-  if (e.target.classList.contains('employ')) {
-    flag = 'employ';
-    const inputsRow = document.querySelector('.inputs__row--2');
-    const rowInputs = inputsRow.querySelectorAll('input');
-    const rowLabels = inputsRow.querySelectorAll('input');
-
-    console.log(inputsRow);
-    console.log(rowInputs);
-    console.log(rowLabels);
-
-    const selectRegionSubmit = document.getElementById('select__region-submit'); //кнопка "Выбрать" регион в попапе
-    const regions = document.querySelectorAll('input[name="select__region"]');
-
-    selectRegionSubmit.onclick = () => {
-      for (let item of regions) {
-        if (item.checked) {
-          showClean();
-        }
-      }
-    };
-    for (let inp of rowInputs) {
-      console.log(inp);
-      inp.addEventListener('input', showClean);
-    }
-  }
-
-  if (e.target.classList.contains('any')) {
-    flag = 'any';
-  }
-
-}
-
-function showClean() {
-  for (let btn of inputsBtnBlues) {
-    if (btn.classList.contains(flag)) {
-      btn.style.display = 'block';
-    }
-    const empl = document.getElementById('employ');
-    if (btn.classList.contains('any')) {
-      empl.style.display = 'block';
-    } //Кнопку Очистить для Поиска работы авторы испортили
+  switch (e.target.getAttribute('data-type')) {
+    case ('staff'):
+      chooseStaff.classList.remove('choose__staff-hide');
+      chooseWork.classList.add('choose__work-hide');
+      break;
+    case ('work'):
+      chooseWork.classList.remove('choose__work-hide');
+      chooseStaff.classList.add('choose__staff-hide');
+      break;
   }
 }
-
-function whereClick(e) {
-  //По умолчанию со старта
-  if (flag == 'any' && e.target.classList.contains('select__option')) {
-    document.getElementById('any').style.display = 'block';
-  }
-
-  let target = e.target;
-  if (target.getAttribute('data-value')) {
-    showClean();
-  }
-}
-
+/*************Конец блока*****************/
 
 //Скрыть/показать поисковую панель
 const rollUp = document.querySelector('.roll-up');
@@ -144,28 +76,272 @@ rollUpBtn.onclick = () => {
   if (flagShow) {
     rollUpSpan.innerText = 'Показать поисковую панель';
     rollUp.classList.add('active');
-    document.querySelector('.tabs__1').classList.add('hide-block');
-    document.querySelector('.tabs__2').classList.add('hide-block');
+    document.querySelector('.choose').classList.add('hide-block');
   } else {
     rollUpSpan.innerText = 'Скрыть поисковую панель';
     rollUp.classList.remove('active');
-    document.querySelector('.tabs__1').classList.remove('hide-block');
-    document.querySelector('.tabs__2').classList.remove('hide-block');
+    document.querySelector('.choose').classList.remove('hide-block');
   }
   flagShow = !flagShow;
 };
-/* Конец блока */
+/*************Конец блока************** */
 
 
-window.onclick = (e) => {
-  console.log(e.target);
-};
 
-const btns = document.querySelectorAll('button');
-for (let b of btns) {
-  if (b.getAttribute('data-value') == 'apply') {
-    b.onclick = () => {
-      b.remove();
-    };
+/***********Работа с полями ввода **********/
+const resetAll = document.querySelector('.reset-all'); //кнопка Очистить
+const inputFields = document.querySelectorAll('.input-field');
+window.addEventListener('click', showInput);
+
+function showInput(e) { //убрать placeholder и показать input
+  if (e.target !== resetAll) {
+    for (let inp of inputFields) {
+      if (inp.value) {
+        inp.parentNode.querySelector('.placeholder').classList.add('hide-block');
+        resetAll.classList.remove('hide-block');
+      } else {
+        inp.parentNode.querySelector('.placeholder').classList.remove('hide-block');
+      }
+    }
   }
 }
+/*************Конец блока************** */
+
+
+/**********Работа с селектами*****************/
+
+const inputSelects = document.querySelectorAll('.inputselect');
+const inputContainerUls = document.querySelectorAll('.input-container__ul');
+const inputContainerItems = document.querySelectorAll('.input-container__item');
+const inputContainerArrows = document.querySelectorAll('.input-container__arrow');
+
+window.addEventListener('click', hideSelect_1); //Показать/убрать список select
+
+function hideSelect_1(e) {
+  //нажали на стрелочку
+  if (e.target.classList.contains('input-container__arrow')) {
+    e.target.parentNode.querySelector('.input-container__ul').classList.toggle('showlist');
+    e.target.previousElementSibling.previousElementSibling.classList.toggle('inputsel');
+    e.target.classList.toggle('arrow-rotate');
+    return;
+  }
+  //нажали вне поля выбора
+  if (!e.target.classList.contains('inputselect')) {
+
+    for (let uls of inputContainerUls) {
+      if (e.target.parentNode.parentNode !== uls) {
+        uls.classList.remove('showlist');
+      }
+    }
+    for (let item of inputSelects) {
+      item.classList.remove('inputsel');
+    }
+    for (let arrow of inputContainerArrows) {
+      arrow.classList.remove('arrow-rotate');
+    }
+  }
+}
+
+//нажали на поле выбора
+for (let inp of inputSelects) {
+  inp.addEventListener('click', hideSelect_2);
+}
+
+function hideSelect_2(e) {
+  //обнуляем остальные поля выбора
+  for (let inp of inputSelects) {
+    inp.classList.remove('inputsel');
+  }
+  for (let uls of inputContainerUls) {
+    uls.classList.remove('showlist');
+    uls.parentNode.querySelector('.arrow').classList.remove('arrow-rotate');
+  }
+
+  e.target.parentNode.querySelector('.input-container__ul').classList.toggle('showlist');
+
+  e.target.classList.toggle('inputsel');
+
+  e.target.nextElementSibling.nextElementSibling.classList.toggle('arrow-rotate');
+}
+
+
+//Показать выбранный select и спрятать placeholder
+for (let item of inputContainerItems) {
+  item.addEventListener('click', showItemSelected);
+}
+
+function showItemSelected(e) {
+  let targ = e.target.parentNode.parentNode.parentNode;
+  if (targ.classList.contains('input-container')) {
+    //обрабатываем мультиселект;
+    // return;
+  }
+  resetAll.classList.remove('hide-block');
+  targ.querySelector('.input-container__ul').classList.toggle('showlist');
+  targ.querySelector('.inputselect').value = this.innerText;
+  targ.querySelector('.placeholder').classList.add('hide-block');
+}
+
+
+/*********Работа с кнопками Применить в мультиселектах*******/
+const applBtns = document.querySelectorAll('.apply');
+for (let btn of applBtns) {
+  btn.addEventListener('click', calculateNumberOfChecked);
+}
+
+function calculateNumberOfChecked(e) {
+  resetAll.classList.remove('hide-block');
+  //В каком мультиселекте нажата копка Применить
+  const eselect = e.target.parentNode;
+  const inputs = eselect.querySelectorAll('input');
+  let counter = 0;
+  for (let inp of inputs) {
+    if (inp.checked) {
+      counter++;
+    }
+  }
+  if (counter) {
+    eselect.parentNode.querySelector('.inputselect').value = 'Выбрано ' + counter;
+    eselect.classList.remove('showlist'); //свернуть список
+  } else {
+    eselect.parentNode.querySelector('.inputselect').value = '';
+    eselect.classList.remove('showlist');
+  }
+}
+/*************Конец блока************** */
+
+
+/*********Управление доп.ползунком в Range******** */
+const salaryMax = 700000;
+const range1 = document.querySelector('.range1');
+const range2 = document.querySelector('.range2');
+const range3 = document.querySelector('.range3');
+const range4 = document.querySelector('.range4');
+range1.value = salaryMax;
+const salary = document.querySelector('.salary');
+const salaryMark = document.querySelector('.salary__mark');
+const salaryMarkWrap = document.querySelector('.salary__mark--wrap');
+let range1Progress;
+var smw = 0;
+const correct = 20; //поправка на размер mark-wrap
+const salMinMax = document.querySelector('.salary-min-max');
+const salMin = document.querySelector('.salary-min');
+const salMax = document.querySelector('.salary-max');
+const salWidthInitial = salMinMax.offsetWidth;
+let salWidth = salary.getBoundingClientRect().right - salary.getBoundingClientRect().left;
+
+range1.style.background = `linear-gradient(to right, #fff 0%, #fff ${0}%, #ec0303 ${0}%, #ec0303 100%)`;
+range2.style.background = `linear-gradient(to right, #fff 0%, #fff ${0}%, #ec0303 ${0}%, #ec0303 100%)`;
+
+salaryMarkWrap.addEventListener('mousedown', handlerMark);
+
+salaryMarkWrap.addEventListener('mouseout', stopHandler);
+
+function stopHandler(e) {
+  salaryMark.classList.remove('salary__mark--active');
+}
+
+function handlerMark(e) {
+  salaryMark.classList.add('salary__mark--active');
+  salaryMarkWrap.onmouseout = () => {
+    salaryMark.classList.remove('salary__mark--active');
+  };
+
+  e.preventDefault();
+  let shiftX = e.clientX - salaryMarkWrap.getBoundingClientRect().left;
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+
+  function onMouseMove(e) {
+
+    salaryMark.classList.add('salary__mark--active');
+
+    let newLeft = e.clientX - shiftX - salary.getBoundingClientRect().left;
+
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+
+    let rightEdge = salary.offsetWidth - salaryMarkWrap.offsetWidth;
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+    salaryMarkWrap.style.left = newLeft - correct + 'px';
+
+    range1Progress = range1.value / salaryMax * 100;
+    smw = (parseFloat(salaryMarkWrap.style.left) + correct) / salWidth * 100;
+    range1.style.background = `linear-gradient(to right, #fff 0%, #fff ${smw}%, #ec0303 ${smw}%, #ec0303 ${range1Progress}%, #fff ${range1Progress}%, #fff 100%)`;
+
+    salMin.innerText = Math.round(smw * 70) * 100;
+
+    salMinMax.style.width = (range1.value - smw * salaryMax / 100) / salaryMax * salWidthInitial + 'px';
+    salMinMax.style.left = smw * salWidthInitial / 100 + 'px';
+  }
+
+  function onMouseUp() {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+    salaryMark.classList.remove('salary__mark--active');
+  }
+
+  salaryMark.ondragstart = function () {
+    return false;
+  };
+}
+
+range1.oninput = () => {
+  range1Progress = range1.value / salaryMax * 100;
+  if (!salaryMarkWrap.style.left) {
+    salaryMarkWrap.style.left = -correct + 'px';
+  }
+  smw = (parseFloat(salaryMarkWrap.style.left) + correct) / salWidth * 100;
+
+  range1.style.background = `linear-gradient(to right, #fff 0%, #fff ${smw}%, #ec0303 ${smw}%, #ec0303 ${range1Progress}%, #fff ${range1Progress}%, #fff 100%)`;
+
+  //Вывод зарплаты с округлением
+  salMinMax.style.width = (range1.value - smw * salaryMax / 100) / salaryMax * salWidthInitial + 'px';
+  salMax.innerText = Math.round(range1.value / 1000) * 1000;
+};
+
+/*************Конец блока************** */
+
+
+
+/***********Работа с кнопкой Очистить**********/
+resetAll.addEventListener('click', resetInputs);
+
+function resetInputs() {
+
+  for (let inp of inputFields) {
+    if (inp.parentNode.querySelector('label').classList.contains('placeholder')) {
+      inp.parentNode.querySelector('label').classList.remove('hide-block');
+    }
+  }
+  for (let inp of inputSelects) {
+    inp.parentNode.querySelector('label').classList.remove('hide-block');
+  }
+
+  range1.style.background = `linear-gradient(to right, #fff 0%, #fff ${0}%, #ec0303 ${0}%, #ec0303 100%)`;
+
+  resetAll.classList.add('hide-block');
+
+  setTimeout(() => {
+    range1.value = salaryMax;
+    salMax.innerText = salaryMax;
+    salMin.innerText = 0;
+    salMinMax.style.width = salWidthInitial + 'px';
+    salMinMax.style.left = 0 + 'px';
+    salaryMarkWrap.style.left = -correct + 'px';
+  }, 10);
+}
+
+/*************Конец блока************** */
+
+
+
+/*************Конец блока************** */
+window.onclick = (e) => {
+
+  //console.log(e.target);
+};
