c4063992d3091b5732298fc8f685315a87ef77d8
Dmitry <krdvmail@mail.ru>
2022-09-15 11:18:34 +0300

    upgrade

--- js/addition.js
+++ js/addition.js
@@ -2564,10 +2564,35 @@ function isDark(e) {
     e.target.parentNode.querySelector('.placeholder') && e.target.parentNode.querySelector('.placeholder').classList.add('white-font');
   }
 
+  const rectPopupEnter = document.querySelector('.popup-enter').getBoundingClientRect();
+  const rectLeft = rectPopupEnter.left;
+  const rectRight = rectPopupEnter.right;
+  const rectTop = rectPopupEnter.top;
+  const rectBottom = rectPopupEnter.bottom;
+  //координаты попапа авторизации для мобильных <720px
+
+  const rectPopupEnter1 = document.querySelector('.popup-enterDesctop').getBoundingClientRect();
+  const rectLeft1 = rectPopupEnter1.left;
+  const rectRight1 = rectPopupEnter1.right;
+  const rectTop1 = rectPopupEnter1.top;
+  const rectBottom1 = rectPopupEnter1.bottom;
+  //координаты попапа авторизации для desctop
+
   if (isShowList(e)) {
-    setTimeout(() => {
-      document.querySelector('.header').classList.remove('body-dark');
-    }, 100);
+    const eX = e.clientX;
+    const eY = e.clientY;
+
+    if (eX < rectLeft || eX > rectRight || eY < rectTop || eY > rectBottom) 
+    {
+    if (eX < rectLeft1 || eX > rectRight1 || eY < rectTop1 || eY > rectBottom1) {
+      if (!e.target.classList.contains('header-enter__btn') && !e.target.classList.contains('submit-ad') && !e.target.classList.contains('btn-enter') && !e.target.classList.contains('btn-regist')) {
+        setTimeout(() => {
+          document.querySelector('.header').classList.remove('body-dark');
+        }, 100);
+      }
+    } 
+   }
+
     document.querySelector('.salary__mark').classList.add('zindex5');
     for (let item of document.querySelectorAll('.zindex50')) {
       item.classList.remove('zindex50');
@@ -3855,9 +3880,241 @@ iframe01.style.height = "470px";
 iframe01.style.overflow = "hidden";
 /******Конец тут будет комментарий при нажатии*******/
 
-/*window.onclick = (e) => {
-  //console.log('target=', e.target);
-  //console.log('currenttarget=', e.currentTarget);
-  //alert(e.clientY);
-  //console.log(document.getElementById('wide1').getBoundingClientRect().bottom);
-};*/
+/*****************Режим авторизации**************/
+const ulUnautorize = document.querySelector('.ul-unautorize');
+const ulAutorize = document.querySelector('.ul-autorize');
+const liAutorize = document.querySelector('.li-autorize');
+const aAutorize = document.querySelector('.a-autorize');
+const autorize = document.querySelector('.autorize');
+const dropdownAutorizes = document.querySelectorAll('.dropdown-autorize');
+const btnEnter = document.querySelector('.btn-enter');
+const btnOut = document.querySelector('.btn-out');
+
+btnEnter.addEventListener('click', function () {
+  ulUnautorize.classList.add('hide-block');
+  ulAutorize.classList.remove('hide-block');
+  autorize.classList.remove('hide-block');
+  liAutorize.classList.remove('lock');
+  aAutorize.classList.remove('lock');
+  for (let item of dropdownAutorizes) {
+    item.classList.remove('hide-block');
+  }
+});
+btnOut.addEventListener('click', function () {
+  ulUnautorize.classList.remove('hide-block');
+  ulAutorize.classList.add('hide-block');
+  autorize.classList.add('hide-block');
+  liAutorize.classList.add('lock');
+  aAutorize.classList.add('lock');
+  for (let item of dropdownAutorizes) {
+    item.classList.add('hide-block');
+  }
+});
+
+/********Слайдер********/
+const popupSlide=document.querySelectorAll('.popup-slide');
+const popupSlideControlChanges = document.querySelectorAll('.popupslide__control-change');
+const slideWidth=470;
+let popupStep = 0;
+let intervalStep;
+
+for (let item of popupSlideControlChanges) {
+ item.addEventListener('click', popupSlideChange);
+}
+
+function popupSlideChange(e) {
+ let targ = e.target.classList;
+ if (targ.contains('popupscc1')) {
+   popupStep = 0;
+ }
+ if (targ.contains('popupscc2')) {
+   popupStep = -1;
+ }
+ if (targ.contains('popupscc3')) {
+   popupStep = -2;
+ }
+ if (targ.contains('popupscc4')) {
+   popupStep = -3;
+ }
+ if (targ.contains('popupscc5')) {
+   popupStep = -4;
+ }
+ if (targ.contains('popupscc6')) {
+   popupStep = -5;
+ }
+ goSlideStep();
+}
+
+function popupSlideShow() {
+  intervalStep = setInterval(changeSlide, 2000);
+}
+
+function changeSlide(item) {
+ if (popupStep > -5) {
+   popupStep--;
+ } else {
+   popupStep = 0;
+ }
+ switch (popupStep) {
+   case 0:
+     document.querySelector('.popupscc1').checked = true;
+     break;
+   case -1:
+     document.querySelector('.popupscc2').checked = true;
+     break;
+   case -2:
+     document.querySelector('.popupscc3').checked = true;
+     break;
+   case -3:
+     document.querySelector('.popupscc4').checked = true;
+     break;
+   case -4:
+     document.querySelector('.popupscc5').checked = true;
+     break;
+   case -5:
+     document.querySelector('.popupscc6').checked = true;
+     break;
+ }
+ goSlideStep();
+}
+
+function goSlideStep() {
+   for (let item of popupSlide) {  
+     item.style.transform = `translateX(${popupStep*slideWidth}px)`;
+     item.style.transition = '0.5s';
+   }
+}
+
+popupSlideShow();
+/*****Конец слайдер*****/
+
+/*****Регистрация******/
+const popupSignin=document.querySelector('a[data-popup="#popup-signin"]');
+const popupEnter_=document.querySelector('a[data-popup="#popup-enter"]');
+
+const miniBtnSignup=document.querySelector('.sigIn-btn');
+const topEnter1=document.querySelector('.top__enter1');
+const topEnter2=document.querySelector('.top__enter2');
+const signinTitle1=document.querySelector('.signin-title1');
+const signinTitle2=document.querySelector('.signin-title2');
+const signup1=document.querySelector('.signup1');
+const signup2=document.querySelector('.signup2');
+const pre1=document.querySelector('.pre1');
+const pre2=document.querySelector('.pre2');
+const popupEnterDesctop=document.querySelector('.popup-enterDesctop');
+const popupEnterDesctopExit=document.querySelector('.popup-enterDesctop__exit');
+
+popupEnter_.addEventListener('click', function(e) {
+ popupEnterDesctop.classList.remove('hide-block');
+ toEnter(e);
+ handlZindex();
+});
+
+popupSignin.addEventListener('click', function(e) {
+ popupEnterDesctop.classList.remove('hide-block');
+ toRegister(e);
+ handlZindex();
+});
+
+popupEnterDesctopExit.onclick=()=> {
+ popupEnterDesctop.classList.add('hide-block');
+ document.querySelector('.header').classList.remove('body-dark');
+};
+
+btnOut.addEventListener('click',()=>{popupEnterDesctop.classList.add('hide-block');
+document.querySelector('.header').classList.remove('body-dark');
+});
+
+miniBtnSignup.addEventListener('click', toRegister);
+
+function toRegister(e) {
+topEnter1.classList.add('hide-block');
+topEnter2.classList.remove('hide-block');
+signinTitle1.classList.add('hide-block');
+signinTitle2.classList.remove('hide-block');
+signup1.classList.add('hide-block');
+signup2.classList.remove('hide-block');
+pre1.classList.add('hide-block');
+pre2.classList.remove('hide-block');
+signinTitle2.classList.remove('hide-block');
+miniBtnSignup.classList.add('hide-block');
+}
+
+signup2.addEventListener('click', toEnter);
+
+function toEnter(e) {
+topEnter1.classList.remove('hide-block');
+topEnter2.classList.add('hide-block');
+signinTitle1.classList.remove('hide-block');
+signinTitle2.classList.add('hide-block');
+signup1.classList.remove('hide-block');
+signup2.classList.add('hide-block');
+pre1.classList.remove('hide-block');
+pre2.classList.add('hide-block');
+miniBtnSignup.classList.remove('hide-block');
+signinTitle2.classList.add('hide-block');
+signup2.classList.add('hide-block');
+}
+
+
+window.addEventListener('click', function(e) {
+   const rectPopupEnter1 = document.querySelector('.popup-enterDesctop').getBoundingClientRect();
+     const rectLeft1 = rectPopupEnter1.left;
+     const rectRight1 = rectPopupEnter1.right;
+     const rectTop1 = rectPopupEnter1.top;
+     const rectBottom1 = rectPopupEnter1.bottom;
+     //координаты попапа авторизации для desctop
+
+   if (!e.target.classList.contains('btn-enter') && !e.target.classList.contains('btn-regist')) {
+   const eX = e.clientX;
+   const eY = e.clientY;
+   
+   if (eX < rectLeft1 || eX > rectRight1 || eY < rectTop1 || eY > rectBottom1) {
+    popupEnterDesctop.classList.add('hide-block');
+   }
+  }
+  });
+
+
+
+/****Конец регистрация****/
+
+/***************Конец режим авторизации***********/
+
+/************Мобильный вариант меньше 720px****** */
+const footerAs = document.querySelectorAll('.footer-a');
+for (let item of footerAs) {
+  item.addEventListener('click', function () {
+    for (let item of footerAs) {
+      item.classList.remove('active');
+    }
+    this.classList.add('active');
+  });
+}
+
+//Авторизация в мобильном варианте
+const submitAd = document.querySelector('.submit-ad');
+const headerEnterBtn = document.querySelector('.header-enter__btn');
+const headerBottomAlert = document.querySelector('.header-bottom__alert');
+const popupEnter = document.querySelector('.popup-enter');
+const popupEnterHide = document.querySelector('.popup-enter__hide');
+
+submitAd.addEventListener('click', autorize720);
+headerEnterBtn.addEventListener('click', autorize720);
+popupEnterHide.addEventListener('click', closeAutorize720);
+
+function autorize720(e) {
+  headerBottomAlert.classList.remove('hide-block');
+  headerEnterBtn.classList.add('hide-block');
+  popupEnter.classList.remove('hide-block');
+  handlZindex();
+}
+
+function closeAutorize720(e) {
+  headerBottomAlert.classList.add('hide-block');
+  headerEnterBtn.classList.remove('hide-block');
+  popupEnter.classList.add('hide-block');
+  document.querySelector('.header').classList.remove('body-dark');
+}
+
+/*********Конец мобильный вариант меньше 720px******/
